61b3d58803ec1de265726eb89180c41596f9d00f
2362201860 <3617147661@qq.com>
2019-03-21 16:03:23 +0800

    cgb-db-sys-v2.01

---
+++ src/main/java/com/db/sys/controller/PageController.java
@@ -0,0 +1,43 @@
+package com.db.sys.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 在此控制层对象中提供一些对外的页面呈现。
+ * @author ta
+ */
+@Controller
+@RequestMapping("/")
+public class PageController {
+	
+	//返回登录页面
+	@RequestMapping("doLoginUI")
+	public String doLoginUI(){
+		return "login";
+	}
+	
+	@RequestMapping("doIndexUI")
+	public String doIndexUI(){
+		return "starter";
+		//返回值会给dispatcherServlet
+		//dispatcherServlet会将此值交给视图解析器
+		//视图解析器会对此值进行解析获取view
+		
+		//Prefix=/WEB-INF/pages/
+		//Suffix=.html
+		//view=/WEB-INF/pages/starter.html
+	}
+	@RequestMapping("doPageUI")
+	public String doPageUI(){
+		return "common/page";
+	}
+}
+
+
+
+
+
+
+
+
